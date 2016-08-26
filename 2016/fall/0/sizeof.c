15499bac473cb6b18bcb6adeb6ffa49e5fce27d8
David J. Malan <dmalan@gmail.com>
2016-08-26 13:42:51 +0000

    week1

---
+++ 2016/fall/0/sizeof.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("bool is %lu\n", sizeof(bool));
+    printf("char is %lu\n", sizeof(char));
+    printf("double is %lu\n", sizeof(double));
+    printf("float is %lu\n", sizeof(float));
+    printf("int is %lu\n", sizeof(int));
+    printf("long long is %lu\n", sizeof(long long));
+    printf("string is %lu\n", sizeof(string));
+}
