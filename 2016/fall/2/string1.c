4ad51bf7dc6271e44ce5f9f08562e1ff4c110d2f
David J. Malan <malan@harvard.edu>
2016-09-05 17:36:47 -0400

    removed string1

--- 2016/fall/2/string1.c
+++ 2016/fall/2/string1.c
@@ -7,7 +7,7 @@ int main(void)
     string s = get_string();
     if (s != NULL)
     {
-        for (int i = 0; i < strlen(s); i++)
+        for (int i = 0, n = strlen(s); i < n; i++)
         {
             printf("%c\n", s[i]);
         }
