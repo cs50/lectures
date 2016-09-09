05727396d0fa14d3c3d6b7706ab233a673bb855a
David J. Malan <malan@harvard.edu>
2016-09-09 08:27:50 -0400

    typo in exit.c

--- 2016/fall/2/exit.c
+++ 2016/fall/2/exit.c
@@ -5,7 +5,7 @@ int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("missing command-line-argument\n");
+        printf("missing command-line argument\n");
         return 1;
     }
     printf("hello, %s\n", argv[1]);
