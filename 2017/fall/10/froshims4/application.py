da470fed98137a2902157b80c8bfa727a4b806fa
David J. Malan <malan@harvard.edu>
2017-10-26 18:33:44 -0400

    added froshims4, froshims5

---
+++ 2017/fall/10/froshims4/application.py
@@ -0,0 +1,36 @@
+from cs50 import SQL
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///froshims4.db")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or not request.form.get("dorm"):
+        return render_template("failure.html")
+    db.execute("INSERT INTO registrants (name, dorm) VALUES(:name, :dorm)",
+               name=request.form.get("name"), dorm=request.form.get("dorm"))
+    return render_template("success.html")
+
+
+@app.route("/registrants")
+def registrants():
+    rows = db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html", registrants=rows)
+
+
+@app.route("/unregister", methods=["GET", "POST"])
+def unregister():
+    if request.method == "GET":
+        rows = db.execute("SELECT * FROM registrants")
+        return render_template("unregister.html", registrants=rows)
+    elif request.method == "POST":
+        if request.form["id"]:
+            db.execute("DELETE FROM registrants WHERE id = :id", id=request.form.get("id"))
+        return redirect("/registrants")
