76c6cbd31a52be29a976c6e6339007962f716801
David J. Malan <malan@harvard.edu>
2018-09-27 20:56:01 -0400

    src2

---
+++ 2/README.md
@@ -0,0 +1,30 @@
+# Week 2
+  
+## Tools
+
+1. `buggy*.c`
+1. `style*.c`
+
+## Arrays
+
+1. `scores*.c`
+
+## Strings
+
+1. `string*.c`
+1. `strlen.c`
+1. `ascii*.c`
+1. `capitalize*.c`
+
+## Command-line Arguments
+
+1. `argv*.c`
+
+## Exit Codes
+
+1. `exit.c`
+
+## Search
+
+1. `linear.c`
+1. `binary*.c`
---
+++ 2/src2/binary0.c
@@ -0,0 +1,51 @@
+// Iterative binary search
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Names in a phone book
+string book[] = {
+    "Chen",
+    "Kernighan",
+    "Leitner",
+    "Lewis",
+    "Malan",
+    "Muller",
+    "Seltzer",
+    "Shieber",
+    "Smith"
+};
+
+int main(void)
+{
+    // Prompt user for name
+    string name = get_string("Name: ");
+
+    // Search for name
+    int left = 0, right = sizeof(book) / sizeof(string) - 1;
+    while (left <= right)
+    {
+        // Look at middle
+        int middle = (left + right) / 2;
+        if (strcmp(name, book[middle]) == 0)
+        {
+            printf("Calling %s\n", name);
+            return 0;
+        }
+
+        // Search left half
+        else if (strcmp(name, book[middle]) < 0)
+        {
+            right = middle - 1;
+        }
+
+        // Search right half
+        else if (strcmp(name, book[middle]) > 0)
+        {
+            left = middle + 1;
+        }
+    }
+    printf("Quitting\n");
+    return 1;
+}
