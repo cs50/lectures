39aec04b6d511f32142615d85bc3db59bb742e5d
David J. Malan <malan@harvard.edu>
2018-10-25 16:34:53 -0400

    style

--- 7/src7/froshims0/application.py
+++ 7/src7/froshims0/application.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/register", methods=["POST"])
 def register():
