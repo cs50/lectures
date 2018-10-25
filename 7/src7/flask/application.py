28b7f0550541993a25ae9fef537859a3e225f4b7
David J. Malan <malan@harvard.edu>
2018-10-25 18:41:49 -0400

    updated hello

--- 7/src7/flask/application.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
---
+++ 7/src7/hello/application.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", name=request.args.get("name", "world"))
