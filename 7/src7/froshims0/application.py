076906b58248829c57ad9226dc35ce71ced9e1c8
David J. Malan <malan@harvard.edu>
2018-10-25 16:16:56 -0400

    added froshims0, 1

---
+++ 7/src7/froshims0/application.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or not request.form.get("dorm"):
+        return render_template("failure.html")
+    return render_template("success.html")
