0332ae27ab1a0c7bc978aeee1333b01549aec75b
David J. Malan <malan@harvard.edu>
2019-09-21 01:24:07 -0400

    mario

---
+++ 3/src3/mario.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void draw(int h);
+
+int main(void)
+{
+    // Get height of pyramid
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1);
+
+    // Draw pyramid
+    draw(height);
+}
+
+void draw(int h)
+{
+    // If nothing to draw
+    if (h == 0)
+    {
+        return;
+    }
+
+    // Draw pyramid of height h - 1
+    draw(h - 1);
+
+    // Draw one more row of width h
+    for (int i = 0; i < h; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
