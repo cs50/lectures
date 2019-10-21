4f23ea53b38e6bc440cfa851ea0a76ee39c7e191
David J. Malan <malan@harvard.edu>
2019-10-21 11:09:30 -0400

    added input()

---
+++ 6/src6/1/int1.py
@@ -0,0 +1,4 @@
+# input, int, and print
+
+age = int(input("What's your age?\n"))
+print(f"You are at least {age * 365} days old.")
---
+++ 6/src6/1/string3.py
@@ -0,0 +1,4 @@
+# input and print, with format strings
+
+s = input("What's your name?\n")
+print(f"hello, {s}")
