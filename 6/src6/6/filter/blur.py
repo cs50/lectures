9115fe819eda01403770b9d523c4757992e112cf
David J. Malan <malan@harvard.edu>
2019-10-21 10:58:31 -0400

    simplified blur

--- 6/src6/6/filter/blur.py
+++ 6/src6/6/filter/blur.py
@@ -1,10 +1,8 @@
 # Blurs an image
 
 from PIL import Image, ImageFilter
-from sys import argv
 
 # Blur image
-if len(argv) == 3:
-    before = Image.open(argv[1])
-    after = before.filter(ImageFilter.BLUR)
-    after.save(argv[2])
+before = Image.open("bridge.bmp")
+after = before.filter(ImageFilter.BLUR)
+after.save("out.bmp")
