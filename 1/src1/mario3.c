5e38ede67dcbd8d2fc0d30b13026ffe2f077ee6a
David J. Malan <malan@harvard.edu>
2020-09-07 17:30:32 -0400

    updated examples

--- 1/src1/mario3.c
+++ 1/src1/mario3.c
@@ -1,10 +1,10 @@
-// Prints a column of 3 bricks
+// Prints 3-by-3 grid of bricks
 
 #include <stdio.h>
 
 int main(void)
 {
-    printf("#\n");
-    printf("#\n");
-    printf("#\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
 }
--- 1/src1/mario4.c
+++ 1/src1/mario4.c
@@ -1,11 +1,16 @@
-// Prints a column of 3 bricks with a loop
+// Prints a 3-by-3 grid of bricks with nested loops
 
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
     for (int i = 0; i < 3; i++)
     {
-        printf("#\n");
+        for (int j = 0; j < 3; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
 }
