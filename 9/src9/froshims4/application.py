c7e5cf8c6e4ce83fbfc1dd572cbeb76e2135e5d4
David J. Malan <malan@harvard.edu>
2020-11-01 22:15:39 -0500

    added sqlite

--- 9/src9/froshims4/application.py
+++ 9/src9/froshims4/application.py
@@ -1,19 +1,11 @@
-# Implements a registration form, confirming registration via email
+# Implements a registration form, storing registrants in a SQLite database
 
-import os
-
-from flask import Flask, render_template, request
-from flask_mail import Mail, Message
-from validate_email import validate_email
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
-app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
-app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
-mail = Mail(app)
+
+db = SQL("sqlite:///froshims.db")
 
 SPORTS = [
     "Dodgeball",
@@ -28,19 +20,33 @@ def index():
     return render_template("index.html", sports=SPORTS)
 
 
+@app.route("/deregister", methods=["POST"])
+def deregister():
+
+    # Forget registrant
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM registrants WHERE id = ?", request.form.get("id"))
+    return redirect("/registrants")
+
+
 @app.route("/register", methods=["POST"])
 def register():
 
     # Validate submission
     name = request.form.get("name")
-    email = request.form.get("email")
     sport = request.form.get("sport")
-    if not name or not validate_email(email) or sport not in SPORTS:
+    if not name or sport not in SPORTS:
         return render_template("failure.html")
 
-    # Send email
-    message = Message("You are registered!", recipients=[email])
-    mail.send(message)
+    # Remember registrant
+    db.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", name, sport)
 
     # Confirm registration
-    return render_template("success.html")
+    return redirect("/registrants")
+
+
+@app.route("/registrants")
+def registrants():
+    registrants = db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html", registrants=registrants)
