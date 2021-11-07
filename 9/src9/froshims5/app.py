3c4de13902445fd18f7d33525fa5cac82224a930
David J. Malan <malan@harvard.edu>
2021-11-07 17:52:30 -0500

    using os.environ

--- 9/src9/froshims5/app.py
+++ 9/src9/froshims5/app.py
@@ -8,12 +8,14 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
-app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+# Requires that "Less secure app access" be on
+# https://support.google.com/accounts/answer/6010255
+app.config["MAIL_DEFAULT_SENDER"] = os.environ["MAIL_DEFAULT_SENDER"]
+app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
 mail = Mail(app)
 
 SPORTS = [
