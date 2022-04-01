aca30a783bd6187edf7d71380ff0d434006d2141
David J. Malan <malan@harvard.edu>
2022-03-31 21:28:52 -0400

    added dict

---
+++ 2/hogwarts3.py
@@ -0,0 +1,13 @@
+# Demonstrates indexing into a dict
+
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Rob": "Gryffindor",
+    "Draco": "Slytherin",
+}
+
+print(students["Hermione"])
+print(students["Harry"])
+print(students["Rob"])
+print(students["Draco"])
---
+++ 2/hogwarts4.py
@@ -0,0 +1,11 @@
+# Demonstrates iterating over and index into a dict
+
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Rob": "Gryffindor",
+    "Draco": "Slytherin",
+}
+
+for student in students:
+    print(student, students[student])
