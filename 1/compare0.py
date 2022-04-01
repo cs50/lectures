68bf506171cdbb0ffff652459f61c6bcc87c6e5e
David J. Malan <malan@harvard.edu>
2022-03-31 21:03:11 -0400

    flipped inequalities in compare*.py

--- 1/compare0.py
+++ 1/compare0.py
@@ -3,9 +3,9 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
-if x > y:
-    print("x is greater than y")
 if x < y:
     print("x is less than y")
+if x > y:
+    print("x is greater than y")
 if x == y:
     print("x is equal to y")
--- 1/compare1.py
+++ 1/compare1.py
@@ -3,9 +3,9 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
-if x > y:
-    print("x is greater than y")
-elif x < y:
+if x < y:
     print("x is less than y")
+elif x > y:
+    print("x is greater than y")
 elif x == y:
     print("x is equal to y")
