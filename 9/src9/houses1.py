6b1fdaff0c9cfb7f82f92f201819be87e863975a
David J. Malan <malan@harvard.edu>
2022-05-11 19:01:15 -0400

    WIP

---
+++ 9/src9/houses1.py
@@ -0,0 +1,16 @@
+# Filters out duplicate houses using set
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+]
+
+houses = set()
+for student in students:
+    houses.add(student["house"])
+
+for house in sorted(houses):
+    print(house)
