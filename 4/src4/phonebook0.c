032cb31566c1cb606e93b9c47d3e5b6586768c0c
David J. Malan <malan@harvard.edu>
2022-09-26 16:48:59 -0400

    added addresses10.c

--- 4/src4/phonebook0.c
+++ 4/src4/phonebook0.c
@@ -8,14 +8,10 @@ int main(void)
 {
     // Open CSV file
     FILE *file = fopen("phonebook.csv", "a");
-    if (!file)
-    {
-        return 1;
-    }
 
     // Get name and number
-    string name = get_string("Name: ");
-    string number = get_string("Number: ");
+    char *name = get_string("Name: ");
+    char *number = get_string("Number: ");
 
     // Print to file
     fprintf(file, "%s,%s\n", name, number);
