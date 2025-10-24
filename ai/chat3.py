9a90bc4a600c406a9c881fa8c0cc07426b1c07dd
David J. Malan <malan@harvard.edu>
2025-10-24 07:03:46 -0400

    ai

---
+++ ai/chat3.py
@@ -0,0 +1,16 @@
+# Adds to system prompt.
+
+from openai import OpenAI
+
+client = OpenAI()
+
+user_prompt = input("Prompt: ")
+system_prompt = "Limit your answer to one sentence. Pretend you're a cat."
+
+response = client.responses.create(
+    input=user_prompt,
+    instructions=system_prompt,
+    model="gpt-5"
+)
+
+print(response.output_text)
